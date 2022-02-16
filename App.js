a6107a73dcc35e3be2813b671d8ef0f885d2889f
Silas White <silas.j.white@gmail.com>
2022-02-15 21:40:11 -0500

    adding navigation packages and updating expo

--- App.js
+++ App.js
@@ -50,7 +50,7 @@ const App = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#AAC2D6",
     alignItems: "center",
     justifyContent: "center",
   },
